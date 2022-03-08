24fe2266965594ff6edbb638e6db60f4d49ad7a0
Dungna89 <dungna29@fpt.edu.vn>
2022-03-08 08:47:03 +0700

    1

--- 5_IT17302_NET101_BLOCK2_SP22/Program.cs
+++ 5_IT17302_NET101_BLOCK2_SP22/Program.cs
@@ -8,6 +8,7 @@ namespace _5_IT17302_NET101_BLOCK2_SP22
 {
     internal class Program
     {
+        //Lý thuyết về biến
         static void Main(string[] args)
         {
             Console.WriteLine("Chao cac ban den voi C#1");
