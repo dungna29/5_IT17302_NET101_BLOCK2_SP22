06198734202cb9c4c5a0d2b9df233a904e7369b3
Dungna89 <dungna29@fpt.edu.vn>
2022-03-08 08:44:06 +0700

    1

---
+++ 5_IT17302_NET101_BLOCK2_SP22/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_IT17302_NET101_BLOCK2_SP22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chao cac ban den voi C#1");
+            
+        }
+    }
+}
